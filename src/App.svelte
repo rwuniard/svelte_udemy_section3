5fdf36987bf2ee535cc750e1c65ac83a2006351b
Ronson Wuniardy <rwuniard@hotmail.com>
2022-03-08 19:48:46 -0500

    Adding button with conditional HTML

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,44 @@
 <script>
-  import ContactCard from "./ContactCard.svelte";
+    import ContactCard from './ContactCard.svelte';
 
-  let name = "Max";
-  let title = "";
-  let image = "";
-  let description = "";
+    let name = 'Max';
+    let title = '';
+    let image = '';
+    let description = '';
+    let done = false;
+
+    function addCard() {
+        done = true;
+    }
 </script>
 
-<style>
-  #form {
-    width: 30rem;
-    max-width: 100%;
-  }
-</style>
-
 <div id="form">
-  <div class="form-control">
-    <label for="userName">User Name</label>
-    <input type="text" bind:value={name} id="userName" />
-  </div>
-  <div class="form-control">
-    <label for="jobTitle">Job Title</label>
-    <input type="text" bind:value={title} id="jobTitle" />
-  </div>
-  <div class="form-control">
-    <label for="image">Image URL</label>
-    <input type="text" bind:value={image} id="image" />
-  </div>
-  <div class="form-control">
-    <label for="desc">Description</label>
-    <textarea rows="3" bind:value={description} id="desc" />
-  </div>
+    <div class="form-control">
+        <label for="userName">User Name</label>
+        <input type="text" bind:value={name} id="userName" />
+    </div>
+    <div class="form-control">
+        <label for="jobTitle">Job Title</label>
+        <input type="text" bind:value={title} id="jobTitle" />
+    </div>
+    <div class="form-control">
+        <label for="image">Image URL</label>
+        <input type="text" bind:value={image} id="image" />
+    </div>
+    <div class="form-control">
+        <label for="desc">Description</label>
+        <textarea rows="3" bind:value={description} id="desc" />
+    </div>
 </div>
 
-<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+<button on:click={addCard}>Add Card</button>
+{#if done}
+    <ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+{/if}
+
+<style>
+    #form {
+        width: 30rem;
+        max-width: 100%;
+    }
+</style>
