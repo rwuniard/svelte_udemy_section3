d3f89803104543fe55074da5e4b5628a260dddae
Ronson Wuniardy <rwuniard@hotmail.com>
2022-03-18 18:01:00 -0400

    event modifier

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,8 @@
     let formState = 'empty';
     let createdContacts = [];
 
-    function addCard() {
+    function addCard(event) {
+        // event.preventDefault(); This can be done using event Modifier |preventDefault in the on:click
         if (
             name.trim().length === 0 ||
             title.trim().length === 0 ||
@@ -48,26 +49,28 @@
     }
 </script>
 
-<div id="form">
-    <div class="form-control">
-        <label for="userName">User Name</label>
-        <input type="text" bind:value={name} id="userName" />
+<form>
+    <div id="form">
+        <div class="form-control">
+            <label for="userName">User Name</label>
+            <input type="text" bind:value={name} id="userName" />
+        </div>
+        <div class="form-control">
+            <label for="jobTitle">Job Title</label>
+            <input type="text" bind:value={title} id="jobTitle" />
+        </div>
+        <div class="form-control">
+            <label for="image">Image URL</label>
+            <input type="text" bind:value={image} id="image" />
+        </div>
+        <div class="form-control">
+            <label for="desc">Description</label>
+            <textarea rows="3" bind:value={description} id="desc" />
+        </div>
     </div>
-    <div class="form-control">
-        <label for="jobTitle">Job Title</label>
-        <input type="text" bind:value={title} id="jobTitle" />
-    </div>
-    <div class="form-control">
-        <label for="image">Image URL</label>
-        <input type="text" bind:value={image} id="image" />
-    </div>
-    <div class="form-control">
-        <label for="desc">Description</label>
-        <textarea rows="3" bind:value={description} id="desc" />
-    </div>
-</div>
+    <button on:click|preventDefault={addCard}>Add Card</button>
+</form>
 
-<button on:click={addCard}>Add Card</button>
 <button on:click={deleteFirst}>Delete First</button>
 <button on:click={deleteLast}>Delete Last</button>
 
